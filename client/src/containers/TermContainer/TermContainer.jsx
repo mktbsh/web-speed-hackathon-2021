d82987f63f5fca6aa70811a07501cf846b006d17
mktbsh <t93.hasebe@gmail.com>
2021-12-12 23:05:01 +0900

    [update] React.lazy

--- client/src/containers/TermContainer/TermContainer.jsx
+++ client/src/containers/TermContainer/TermContainer.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
 import { TermPage } from '../../components/term/TermPage';
 
 /** @type {React.VFC} */
 const TermContainer = () => {
-  return (
-    <>
-      <Helmet>
-        <title>利用規約 - CAwitter</title>
-      </Helmet>
-      <TermPage />
-    </>
-  );
+  return <TermPage />;
 };
 
 export { TermContainer };
