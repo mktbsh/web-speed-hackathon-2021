8bc03c559b6223a1946aecc30f7f2ceb318fab31
mktbsh <t93.hasebe@gmail.com>
2021-12-12 20:17:45 +0900

    Update seeds.js

--- server/src/seeds.js
+++ server/src/seeds.js
@@ -9,15 +9,25 @@ import users from '../seeds/users.json';
 
 import { Comment, Image, Movie, Post, PostsImagesRelation, ProfileImage, Sound, User } from './models';
 
+import { performance } from "perf_hooks";
+
+function measure(name, size, start, end) {
+  console.log(`${size} ${name} inserted in ${(end - start)} milliseconds`);
+}
+
 async function insertSeeds() {
   await ProfileImage.bulkCreate(profileImages, { logging: false });
   await Image.bulkCreate(images, { logging: false });
   await Movie.bulkCreate(movies, { logging: false });
   await Sound.bulkCreate(sounds, { logging: false });
-  await User.bulkCreate(users, { logging: false });
+  await User.bulkCreate(users.map(([id, username, name, description, password, profileImageId, createdAt]) => ({
+    id, username, name, description, password, profileImageId, createdAt
+  })), { logging: false });
   await Post.bulkCreate(posts, { logging: false });
-  await PostsImagesRelation.bulkCreate(postsImagesRelation, { logging: false });
-  await Comment.bulkCreate(comments, { logging: false });
+  await PostsImagesRelation.bulkCreate(postsImagesRelation.map(([postId, imageId]) => ({ postId, imageId })), { logging: false });
+  await Comment.bulkCreate(comments.map(([id, userId, postId, text, createdAt]) => ({
+    id, userId, postId, text, createdAt
+  })), { logging: false });
 }
 
 export { insertSeeds };
