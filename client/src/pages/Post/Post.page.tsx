a80b17385e42a9ca85fb4565ed220131835298a9
mktbsh <t93.hasebe@gmail.com>
2021-12-25 16:11:00 +0900

    Update Post.page.tsx

--- client/src/pages/Post/Post.page.tsx
+++ client/src/pages/Post/Post.page.tsx
@@ -1,11 +1,16 @@
 import { PostPageItem } from '../../components/Post/PostPageItem';
 import { useMatch } from 'react-location';
 import { LocationGenerics } from '../../Router';
+import { useSetHelmet } from '../../contexts/HelmetContext';
 
 export const PostPage = () => {
   const {
     data: { post },
   } = useMatch<LocationGenerics>();
+
+  useSetHelmet({
+    title: post ? `${post.user.name} さんのつぶやき` : undefined,
+  });
 
   if (!post) return null;
 
