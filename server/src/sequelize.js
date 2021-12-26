7f527ca3ac325b4d31adbb832f077ea3ae635be1
mktbsh <t93.hasebe@gmail.com>
2021-12-26 21:21:13 +0900

    Update sequelize.js

--- server/src/sequelize.js
+++ server/src/sequelize.js
@@ -6,6 +6,12 @@ const sequelize = new Sequelize({
   dialect: 'sqlite',
   logging: false,
   storage: DATABASE_PATH,
+  pool: {
+    max: 4,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 export { sequelize };
